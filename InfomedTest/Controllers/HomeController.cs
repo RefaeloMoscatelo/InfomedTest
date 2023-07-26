237f69bf29d96027b8b0fdc608354b0cb60a13d2
Rafael <refaelomoscatelo@gmail.com>
2023-07-27 00:21:32 +0300

    Add project files.

---
+++ InfomedTest/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+using InfomedTest.Models;
+using Newtonsoft.Json.Linq;
+
+namespace InfomedTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Doctors()
+        {
+
+            var path = Server.MapPath(@"~/Models/JsonData/doctors.json");
+
+            using (StreamReader r = new StreamReader(path))
+            {
+                string Doctorsjson = r.ReadToEnd();
+                dynamic items = JsonConvert.DeserializeObject<List<Root>>(Doctorsjson)
+                  .Where(x => x.isActive == true)
+                  .Where(x => x.promotionLevel <= 5)
+                  .OrderBy(x => x.reviews.averageRating)
+                 .ThenByDescending(x => x.reviews.totalRatings)
+                 .ThenByDescending(x => x.promotionLevel).ToList();
+            }
+
+            return View();
+        }
+       
+    }
+}
---
+++ InfomedTest/Models/Doctor.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfomedTest.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class Address
+    {
+        public string cityName { get; set; }
+        public string street { get; set; }
+        public string cityId { get; set; }
+    }
+
+    public class Clinic
+    {
+        public int id { get; set; }
+        public Address address { get; set; }
+        public DateTime dateCreated { get; set; }
+        public string name { get; set; }
+        public List<Phone> phones { get; set; }
+        public bool isActive { get; set; }
+    }
+
+    public class Phone
+    {
+        public string number { get; set; }
+        public int phoneType { get; set; }
+    }
+
+    public class Reviews
+    {
+        public int professionalismRate { get; set; }
+        public int averageRating { get; set; }
+        public int totalRatings { get; set; }
+        public int waitingTimeRate { get; set; }
+        public int serviceRate { get; set; }
+    }
+
+    public class Root
+    {
+        public int id { get; set; }
+        public int promotionLevel { get; set; }
+        public List<Clinic> clinics { get; set; }
+        public Reviews reviews { get; set; }
+        public List<string> mainSpecialtiesIds { get; set; }
+        public List<Phone> phones { get; set; }
+        public List<string> subSpecialtiesIds { get; set; }
+        public List<string> languageIds { get; set; }
+        public string fullName { get; set; }
+        public List<string> areaIds { get; set; }
+        public bool isActive { get; set; }
+        public List<string> mainSpecializationsIds { get; set; }
+        public List<string> subSpecializationsIds { get; set; }
+    }
+
+
+}
