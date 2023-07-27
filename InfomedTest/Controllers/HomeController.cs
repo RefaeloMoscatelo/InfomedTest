345b94c469a630e4a1b0cae8480a7fc0bd9d3202
Rafael <refaelomoscatelo@gmail.com>
2023-07-27 04:49:48 +0300

    View Created

--- InfomedTest/Controllers/HomeController.cs
+++ InfomedTest/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.IO;
 using Newtonsoft.Json;
 using InfomedTest.Models;
 using Newtonsoft.Json.Linq;
+using System.Web.Script.Serialization;
 
 namespace InfomedTest.Controllers
 {
@@ -34,7 +35,7 @@ namespace InfomedTest.Controllers
         public ActionResult Doctors()
         {
             var path = Server.MapPath(@"~/Models/JsonData/doctors.json");
-            
+
             using (StreamReader r = new StreamReader(path))
             {
                 string Doctorsjson = r.ReadToEnd();
@@ -43,30 +44,63 @@ namespace InfomedTest.Controllers
                   .Where(x => x.promotionLevel <= 5)
                   .OrderBy(x => x.reviews.averageRating)
                  .ThenByDescending(x => x.reviews.totalRatings)
-                 .ThenByDescending(x => x.promotionLevel).Select(x => new Doctor {
+                 .ThenByDescending(x => x.promotionLevel).Select(x => new Doctor
+                 {
                      ID = x.id,
                      FullName = x.fullName,
                      Rating = x.reviews.averageRating,
-                     Languages = null,
+                     Languages = x.languageIds,
                      isArticle = false,
                      PhoneNumber = x.phones[0].number
                  }).ToList();
                 var list = FixPhones(items);
+                list = CheckIfArticle(list);
+                return View(list);
             }
-            
-          
-
 
             
-            return View();
+            
         }
-       public List<Doctor> FixPhones(List<Doctor> list)
+
+        //private List<Doctor> AddLanguages(List<Doctor> list)
+        //{
+        //    var path = Server.MapPath(@"~/Models/JsonData/language.json");
+            
+
+
+
+
+        //    using (StreamReader r = new StreamReader(path))
+        //    {
+        //        string languages = r.ReadToEnd();
+        //        var jsSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+
+        //        Dictionary<string, object> dict = (Dictionary<string, object>)jsSerializer.DeserializeObject(languages);
+        //        var jss = new JavaScriptSerializer();
+        //        var table = jss.Deserialize<dynamic>(languages);
+
+        //        foreach (var doctor in list)
+        //        {
+        //            foreach (var language in table)
+        //            {
+        //                if (true)
+        //                {
+
+        //                }
+        //            }
+        //        }
+
+        //    }
+        //    return list;
+        //}
+
+        private List<Doctor> FixPhones(List<Doctor> list)
         {
             foreach (var item in list.Where(w => !w.PhoneNumber.Contains("-")))
             {
-                if ((item.PhoneNumber.StartsWith("07"))|| (item.PhoneNumber.StartsWith("05")))
+                if ((item.PhoneNumber.StartsWith("07")) || (item.PhoneNumber.StartsWith("05")))
                 {
-                    item.PhoneNumber= item.PhoneNumber.Insert(3, "-");
+                    item.PhoneNumber = item.PhoneNumber.Insert(3, "-");
                 }
                 else
                 {
@@ -74,6 +108,34 @@ namespace InfomedTest.Controllers
                 }
             }
 
+            return list;
+        }
+
+        private List<Doctor> CheckIfArticle(List<Doctor> list)
+        {
+            var path = Server.MapPath(@"~/Models/JsonData/articles.json");
+
+            using (StreamReader r = new StreamReader(path))
+            {
+                string articles = r.ReadToEnd();
+                dynamic articlesItems = JsonConvert.DeserializeObject<List<RootArticle>>(articles);
+                foreach (var doctor in list)
+                {
+                    foreach (var article in articlesItems)
+                    {
+                        if (article.sponsorships.Count == 0)
+                        {
+                            continue;
+                        }
+                        else if(doctor.ID== article.sponsorships[0].sponsorshipId)
+                        {
+                            doctor.isArticle = true;
+                            break;
+                        }
+                    }
+                }
+                
+            }
             return list;
         }
     }
---
+++ InfomedTest/Models/Articles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfomedTest.Models
+{
+    public class RootArticle
+    {
+        public int id { get; set; }
+        public string authorName { get; set; }
+        public string description { get; set; }
+        public DateTime dateModified { get; set; }
+        public string title { get; set; }
+        public bool isActive { get; set; }
+        public int views { get; set; }
+        public List<string> specializationsIds { get; set; }
+        public List<Sponsorship> sponsorships { get; set; }
+        public int? intId { get; set; }
+    }
+
+    public class Sponsorship
+    {
+        public string sponsorshipName { get; set; }
+        public int sponsorshipType { get; set; }
+        public int sponsorshipId { get; set; }
+        public string promotionText { get; set; }
+    }
+}
